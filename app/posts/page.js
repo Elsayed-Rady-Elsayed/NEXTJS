4e1e9001ca37ab2d091142fac8f9be809db6957f
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-05 01:10:09 +0200

    added

--- app/posts/page.js
+++ app/posts/page.js
@@ -2,7 +2,7 @@
 
 const Posts = async () => {
   const result = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
-    cache: "no-store",
+    cache: "force-cache",
   })
     .then((response) => response.json())
     .then((json) => console.log(json));
