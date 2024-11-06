61addb7e9db0481251850fdf0b14dd13f80de5a4
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-06 03:34:22 +0200

    add some redux

--- app/cart/page.js
+++ app/cart/page.js
@@ -5,8 +5,27 @@ import { useSelector } from "react-redux";
 
 const Page = () => {
   const cart = useSelector((state) => state.cart.cart);
-
-  return <div>cart</div>;
+  const items = cart.map((data) => {
+    return (
+      <div className="">
+        <div className=" w-full h-[50%] flex flex-col md:flex-row items-center justify-center gap-3">
+          <img src={data.image} alt="" className="h-96" />
+          <div className="px-6 py-4 flex flex-col gap-5">
+            <h3 className="text-xl font-semibold text-gray-900 line-clamp-2">
+              {data.title}
+            </h3>
+            <p className="text-gray-600 text-base mt-2 line-clamp-2">
+              {data.description}
+            </p>
+            <button className="bg-red-400 text-white p-2 rounded-md">
+              remove
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  });
+  return <div className="grid grid-cols-2 items-center gap-5">{items}</div>;
 };
 
 export default Page;
